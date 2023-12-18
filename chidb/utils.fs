4504357adbace1cff217b5de0fc4c795584cd5d3
dsachitano <david@aten.sky.pod>
2023-12-18 10:32:06 -0500

    fix multibyte write
    * not sure why, but it had a bug where it was writing one more byte
      out after the end of the loop.  noticed it during testing, and
      now we just drop the addr and char val from the stack before returning

--- chidb/utils.fs
+++ chidb/utils.fs
@@ -35,6 +35,7 @@
     \ the iterator I will indicate how many bits (8 * I) we need to shift by
     \ so that the final spot in the buffer is the final byte of the number
     -DO
+        \ DEBUG PRINTOUTS ." LOOP ITER " I . ." stack: " .s cr 
                     \ (acc addr -- ) start here
         swap        \ (addr acc -- )
         dup         \ (addr acc acc -- )
@@ -46,9 +47,12 @@
         dup         \ (acc thisByte addr addr -- )
         rot         \ (acc addr addr thisByte -- )
         swap        \ (acc addr thisByte addr -- )
+        \ DEBUG PRINTOUTS ."     GOING TO WRITE " 2dup swap hex. ." TO ADDR " hex. cr 
         c!          \ (acc addr -- )
         1 +         \ (acc offsetAddr -- )
     1 -LOOP
 
-    c!              \ at the end of the loop, write the final byte to the addr
+    \ not sure why this code was originally writing out one more byte, but that seems wrong.
+    \ now just drop the final char and addr from the stack and move on
+    drop drop 
 ;
